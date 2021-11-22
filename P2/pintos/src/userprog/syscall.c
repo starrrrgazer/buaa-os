623bd39403c4fb834759c5a2a910184e963ce87f
starrrrgazer <70183278+starrrrgazer@users.noreply.github.com>
2021-11-22 17:38:26 +0800

    对参数内存地址的合法判断checkPtr

--- P2/pintos/src/userprog/syscall.c
+++ P2/pintos/src/userprog/syscall.c
@@ -9,9 +9,10 @@
 #include "devices/input.h"
 #include "devices/shutdown.h"
 #include "process.h"
-
+#include "threads/synch.h"
 static void syscall_handler (struct intr_frame *);
 typedef void (*CALL_PROC)(struct intr_frame*);
+static struct lock filelock = ;
 CALL_PROC syscalls[21];
 void write(struct intr_frame* f); /* syscall write */
 void halt(struct intr_frame* f); /* syscall halt. */
@@ -58,10 +59,64 @@ syscall_handler (struct intr_frame *f UNUSED)
   int type = * (int *)f->esp;//检验系统调用号sys_code是否合法
   if(type <= 0 || type >= 21){
     printf ("system error!\n");
+    /*wll update. 对于错误，返回应该是-1*/
+    thread_current()->exitStatus = -1;
     thread_exit ();
   }
   syscalls[type](f);
 }
+
+/* wll update.
+* 判断指针指向的地址是否合法.用法就是在取参数前，把指向参数的指针传进来检查这个是不是合法
+* 包括需要判断是否是用户地址；系统调用函数的范围[0,12](这个在handler里已经判断了)
+* 如果一个指针，指向了没有权限的位置，就应该直接以-1退出。文档的3.1.5部分给我们提供了两种思路，一种是userprog/pagedir.c 和 in threads/vaddr.h中的相关函数pagedir_get_page，来验证地址范围，一种是通过访问该地址，来造成page_fault，在page_fault处理函数中，再退出。
+* 采用第二种方法，还需修改page_fault
+* */
+void * checkPtr(const void *user_ptr){
+  //是否指向了没有权限的位置
+  if(!is_user_vaddr(user_ptr)){ 
+    thread_current()->exitStatus = -1;
+    thread_exit();
+  }
+  //采用第二种方法，检查用户指针是否指向下方PHYS_BASE，然后取消引用它.
+  uint8_t * uaddr = (uint8_t *)user_ptr;
+  for(uint8_t i = 0; i<4;i++){
+    if(get_user(uaddr + i)==-1){
+      thread_current()->exitStatus = -1;
+      thread_exit();
+    }
+  }
+}
+/*下面是官方文档里给出的帮助代码*/
+
+/* 在用户虚拟地址 UADDR 读取一个字节。
+   UADDR 必须低于 PHYS_BASE。
+   如果成功则返回字节值，如果出现段错误则返回 -1
+   发生了。*/
+static int
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+ 
+/* 将 BYTE 写入用户地址 UDST。
+   UDST 必须低于 PHYS_BASE。
+   如果成功则返回真，如果发生段错误则返回假。*/
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
+//这些函数中的每一个都假定用户地址已经被验证为如下PHYS_BASE。他们还假设您已经进行了修改，page_fault()以便内核中的页面错误仅设置eax为0xffffffff并将其以前的值复制到eip.
+
+
+
 void 
 write (struct intr_frame* f)
 {
@@ -94,6 +149,7 @@ void exec(struct intr_frame* f){uint32_t *user_ptr = f->esp;}
 * */
 void wait(struct intr_frame* f){
   uint32_t *user_ptr = f->esp;
+  checkPtr(user_ptr + 1 );
   user_ptr ++;
   f->eax = process_wait(*user_ptr);
 }
@@ -103,8 +159,12 @@ void wait(struct intr_frame* f){
  * */
 void create(struct intr_frame* f){
     uint32_t *user_ptr = f->esp; 
+    checkPtr(user_ptr + 1);
+    checkPtr(user_ptr + 2);
     user_ptr++;
+    lock_acquire(&filelock);
     f->eax = filesys_create((const char*)*user_ptr,*(user_ptr+1));
+    lock_release(&filelock);
 }
 /*
  * 在P2/pintos/src/filesys/filesys.c 里面有个
@@ -112,8 +172,11 @@ void create(struct intr_frame* f){
  * */
 void remove(struct intr_frame* f){
   uint32_t *user_ptr = f->esp;
+  checkPtr(user_ptr + 1);
   user_ptr++;
+  lock_acquire(&filelock);
   f->eax = filesys_remove((const char*)*user_ptr);
+  lock_release(&filelock);
   }
 
 void open(struct intr_frame* f){uint32_t *user_ptr = f->esp;}
